8b0191b04a8edda4b34db313e38c69c78baa5905
doh_dav <dohdav@gmail.com>
2019-11-15 16:11:52 +0900

    debut false etc

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(lq6p7k#r35--s1b@i*325^9--qe%@(v&@-o^ml$-xf_e+)#1^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
